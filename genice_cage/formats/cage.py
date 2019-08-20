d12d28d141769e466d59cbb3d3da0e40af2f4c0f
Masakazu Matsumoto <vitroid@gmail.com>
2019-08-20 19:07:13 +0900

    .

--- genice_cage/formats/cage.py
+++ genice_cage/formats/cage.py
@@ -12,33 +12,34 @@ It may not work with a small structure. (In the example above, the unit cell of 
 Options:
     Cage sizes to be listed, separated by commas and ranged with hyphens. (default is 3 to 8)
     maxring=x  Specify the maximum ring size (default=8).
-    json       Output in JSON format.
+    json       Output in [JSON](https://www.json.org/) format.
 """
 
 import networkx as nx
 from countrings import countrings_nx as cr
 from genice_cage.polyhed import Polyhed
 import json
-import sys
 import numpy as np
 from logging import getLogger
 
 def centerOfMass(members, rpos):
     logger = getLogger()
-    com = rpos[members[0]].copy()
     dsum = np.zeros(3)
     for member in members:
-        d = rpos[member] - com
+        d = rpos[member] - rpos[members[0]]
         d -= np.floor(d+0.5)
         dsum += d
-    com += dsum / len(members)
+    com = rpos[members[0]] + dsum / len(members)
     com -= np.floor(com)
     return com
 
+
 def hook2(lattice):
     global options
+
     logger = getLogger()
     logger.info("Hook2: Cages and vitrites")
+
     cell = lattice.repcell 
     positions = lattice.reppositions
     graph = nx.Graph(lattice.graph) #undirected
@@ -66,14 +67,18 @@ def hook2(lattice):
             for ringid in sorted(cage):
                 print("  Ring {0}: ({1}, {2}, {3}) {4} gon".format(ringid, *ringpos[ringid], len(ringlist[ringid])))
                 print("    Nodes: {0}".format(ringlist[ringid]))
+
     logger.info("Hook2: end.")
 
 
 def argparser(lattice, arg):
     global options
+
     logger = getLogger()
     logger.info("Hook0: Parse options for cage plugin.")
+
     options={"sizes":set(), "maxring":8, "json":False}
+
     if arg != "":
         for a in arg.split(":"):
             decl = a.split("=")
@@ -97,10 +102,13 @@ def argparser(lattice, arg):
                             options["sizes"].add(x)
                     else:
                         options["sizes"].add(int(v))
+
     if len(options["sizes"]) == 0:
         options["sizes"] = {3,4,5,6,7,8}
+
     logger.info("  Max ring size: {0}".format(options["maxring"]))
     logger.info("  Cage sizes:    {0}".format(options["sizes"]))
+
     logger.info("Hook0: end.")
 
 hooks = {0:argparser, 2:hook2}
