ee8ac4c82ceadba7bf4cfcd6aa2c15c9b0c91251
Masakazu Matsumoto <vitroid@gmail.com>
2019-09-02 16:25:48 +0900

    Quadcage order parameter.
    [JMM2011] Jacobson, L. C., Matsumoto, M. & Molinero, V. Order parameters for the multistep crystallization of clathrate hydrates. J. Chem. Phys. 135, 074501 (2011).[doi:10.1063/1.3613667](https://doi.org/10.1063/1.3613667)

--- genice_cage/formats/cage.py
+++ genice_cage/formats/cage.py
@@ -8,21 +8,27 @@ Usage:
     % genice CRN1 -f cage[3-10:json] 
     % genice CRN1 -f cage[3-10:yaplot] 
     % genice CS2 -w tip4p -f cage[gromacs:-16:maxring=6]
+    % analice traj.gro -O OW -H HW[12] -w tip4p -f cage[quad]
+    % analice traj.gro -O OW -H HW[12] -w tip4p -f cage[quad:json]
 
 It may not work with a small structure. (In the example above, the unit cell of CS1 is extended to 2x2x2 so as to avoid detecting cell-spanning wierd cages.)
 
 Options:
     Cage sizes to be listed, separated by commas and ranged with hyphens. (e.g. -4,6,8-10,16-) (default is 3-8)
     maxring=x  Specify the maximum ring size (default=8).
-    json       Output in [JSON](https://www.json.org/) format.
-    yaplot     Visualize with [Yaplot](https://github.com/vitroid/Yaplot/). Cages are drawn in different layers according to the number of faces, and faces are colored according to the number of vertices.
-    gromacs    Output in Gromacs format. (EXPERIMENTAL)
+    json       Output values in [JSON](https://www.json.org/) format.
+    yaplot     Visualize cages with [Yaplot](https://github.com/vitroid/Yaplot/). Cages are drawn in different layers according to the number of faces, and faces are colored according to the number of vertices.
+    gromacs    Output individual cages in Gromacs format. (EXPERIMENTAL)
+    quad       Quadcage order parameter to identify the Frank-Kasper-type crystal structures.[JMM2011] Cages sizes and maximum ring size are set appropriately automatically.
+
+* [JMM2011] Jacobson, L. C., Matsumoto, M. & Molinero, V. Order parameters for the multistep crystallization of clathrate hydrates. J. Chem. Phys. 135, 074501 (2011).[doi:10.1063/1.3613667](https://doi.org/10.1063/1.3613667)
 """
 
 # standard modules
 import json
 from logging import getLogger
 from collections import defaultdict
+from math import log2
 
 # external modules
 import networkx as nx
@@ -84,7 +90,52 @@ def hook2(lattice):
         logger.info("Hook2: end.")
         return
 
-    if options.json:
+    if options.quad:
+        oncage = defaultdict(list)
+        for cage in cages:
+            nodes = set()
+            for ringid in cage:
+                nodes |= set(ringlist[ringid])
+            for node in nodes:
+                oncage[node].append(len(cage))
+        op = dict()
+        for node in oncage:
+            count = [0 for i in range(17)]
+            for v in oncage[node]:
+                count[v] += 1
+            v = "{0}{1}{2}{3}".format(count[12], count[14], count[15], count[16])
+            op[node] = v
+            
+        stat = defaultdict(int)
+        for node in sorted(op):
+            v = op[node]
+            stat[v] += 1
+
+        if options.json:
+            output = dict()
+            N = positions.shape[0]
+            output["op"] = {str(k):v for k,v in op.items()}
+            output["stat"] = {k:v/N for k,v in stat.items()}
+            print(json.dumps(output, indent=2, sort_keys=True))
+        else:
+            for node in sorted(op):
+                print(node, op[node])
+            print("# Statistics")
+            for v in sorted(stat):
+                print("{0} {1} {2}/{3}".format(v, stat[v]/positions.shape[0], stat[v], positions.shape[0]))
+        #ideal = {"CS2": {"2002": 0.7058823529411765,
+        #                 "3001": 0.23529411764705882,
+        #                 "4000": 0.058823529411764705},
+        #         "CS1": {"0400": 0.13043478260869565,
+        #                 "1300": 0.8695652173913043},
+        #}
+        #for ref in ideal:
+        #    dKL = 0
+        #    for v in sorted(stat):
+        #        if v in ideal[ref]:
+        #            dKL += ideal[ref][v]*(log2(ideal[ref][v]) - log2(stat[v]/positions.shape[0]))
+        #    print("{0} dKL={1}".format(ref, dKL))
+    elif options.json:
         output = dict()
         output["rings"] = ringlist
         output["cages"] = cages
@@ -171,7 +222,13 @@ def argparser(lattice, arg):
     logger = getLogger()
     logger.info("Hook0: Parse options for cage plugin.")
 
-    options=AttrDict({"sizes":set(), "maxring":8, "json":False, "gromacs":False, "yaplot":False})
+    options=AttrDict({"sizes":set(),
+                      "maxring":8,
+                      "json":False,
+                      "gromacs":False,
+                      "yaplot":False,
+                      "quad":False,
+    })
 
     if arg != "":
         for a in arg.split(":"):
@@ -187,6 +244,10 @@ def argparser(lattice, arg):
                 options.gromacs = True
             elif a in ("yaplot",):
                 options.yaplot = True
+            elif a in ("quad",):
+                options.quad = True
+                options.sizes = set([12,14,15,16])
+                options.maxring = 6
             else:
                 # value list for cage sizes
                 for v in a.split(","):
